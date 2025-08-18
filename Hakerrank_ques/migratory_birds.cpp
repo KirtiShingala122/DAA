64c82e6b7c1c6cb5a3d70a58d8a0095142e689fb
KirtiShingala122 <kirtishingala122@gmail.com>
2025-08-18 17:39:37 +0530

    Update migratory_birds.cpp

--- Hakerrank_ques/migratory_birds.cpp
+++ Hakerrank_ques/migratory_birds.cpp
@@ -1,45 +1,63 @@
 int migratoryBirds(vector<int> arr) {
-     int a=0,b=0,c=0,d=0,e=0;
-     for(int i=0;i<arr.size();i++){
-         if(arr[i]==1){
-             a++;
-         }
-          if(arr[i]==2){
-             b++;
-         }
-          if(arr[i]==3){
-             c++;
-         }
-          if(arr[i]==4){
-             d++;
-         }
-          if(arr[i]==5){
-             e++;
-         }
-            
-     }
-     int max=0;
-        max = (a > b)? a : b;
-        max = (max > c)? max : c;
-        max = (max > d)? max : d;
-        max = (max > e)? max : e;
-        
-        if(max==a){
-            return 1;
+    unordered_map<int, int> freq;
+ for (int num : arr) {
+        freq[num]++;
+    }
+     int maxFreq = 0, ans = 0;
+
+    for (auto &p : freq) {
+        if (p.second > maxFreq || (p.second == maxFreq && p.first < ans)) {
+            maxFreq = p.second;
+            ans = p.first;
         }
-        if(max==b){
-            return 2;
-        }
-        if(max==c){
-            return 3;
-        }
-        if(max==d){
-            return 4;
-        }
-        if(max==e){
-            return 5;
-        }
-     
-      return max;  
-     
+    }
+
+    return ans;
 }
+
+
+// int migratoryBirds(vector<int> arr) {
+//      int a=0,b=0,c=0,d=0,e=0;
+//      for(int i=0;i<arr.size();i++){
+//          if(arr[i]==1){
+//              a++;
+//          }
+//           if(arr[i]==2){
+//              b++;
+//          }
+//           if(arr[i]==3){
+//              c++;
+//          }
+//           if(arr[i]==4){
+//              d++;
+//          }
+//           if(arr[i]==5){
+//              e++;
+//          }
+            
+//      }
+//      int max=0;
+//         max = (a > b)? a : b;
+//         max = (max > c)? max : c;
+//         max = (max > d)? max : d;
+//         max = (max > e)? max : e;
+        
+//         if(max==a){
+//             return 1;
+//         }
+//         if(max==b){
+//             return 2;
+//         }
+//         if(max==c){
+//             return 3;
+//         }
+//         if(max==d){
+//             return 4;
+//         }
+//         if(max==e){
+//             return 5;
+//         }
+     
+//       return max;  
+     
+// }
