07e666649846ebeb581dd4e5d5d43f50a749a7c7
KirtiShingala122 <kirtishingala122@gmail.com>
2025-08-20 00:06:34 +0530

    Update cut_the_sticks.cpp

--- Hakerrank_ques/cut_the_sticks.cpp
+++ Hakerrank_ques/cut_the_sticks.cpp
@@ -1,13 +1,23 @@
 vector<int> cutTheSticks(vector<int> arr) {
-vector<int> ans;
-sort(arr.begin(),arr.end());
-     while (!arr.empty()) {
-        ans.push_back(arr.size()); 
-        vector<int> temp;
-        for (int x : arr) {
-            if (x - arr[0] > 0) temp.push_back(x - arr[0]);
+// vector<int> ans;
+// sort(arr.begin(),arr.end());
+//      while (!arr.empty()) {
+//         ans.push_back(arr.size()); 
+//         vector<int> temp;
+//         for (int x : arr) {
+//             if (x - arr[0] > 0) temp.push_back(x - arr[0]);
+//         }
+//         arr = temp;  
+//     }
+//     return ans;
+
+       vector<int> ans;
+      sort(arr.begin(),arr.end());
+      ans.push_back(arr.size());
+   for(int i=1;i<arr.size();i++){
+        if(arr[i-1]!=arr[i]) {
+           ans.push_back(arr.size()-i);
         }
-        arr = temp;  
     }
     return ans;
 }
